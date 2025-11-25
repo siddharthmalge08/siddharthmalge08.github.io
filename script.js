5ee6a3e6d23bcd269bfaf03dd804c2d3efa6ee81
siddharthmalge08 <siddharthmalge08@gmail.com>
2025-11-25 20:45:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-
-// Simple front-end form handler (no server)
-document.getElementById('contactForm').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('Thanks! Message captured locally. Implement server endpoint to receive messages.');
-    this.reset();
-});
+console.log('Full site loaded');
